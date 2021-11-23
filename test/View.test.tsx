f41de36ce7b286d3a7bdb94193cf20dae123d708
limichange <limichange@hotmail.com>
2021-11-23 10:29:21 +0800

    feat: use toMatchSnapshot

--- test/View.test.tsx
+++ test/View.test.tsx
@@ -5,39 +5,17 @@ import { View } from '../src'
 test('<View />', async () => {
   const view = render(<View />)
 
-  expect(view.container).toMatchInlineSnapshot(`
-    <div>
-      <div
-        class="emotion-0"
-      />
-    </div>
-  `)
+  expect(view.container).toMatchSnapshot()
 })
 
 test(`<View as='button' />`, () => {
   const view = render(<View as='button' />)
 
-  expect(view.container).toMatchInlineSnapshot(`
-    <div>
-      <button
-        class="emotion-0"
-      />
-    </div>
-  `)
+  expect(view.container).toMatchSnapshot()
 })
 
 test(`<View position='absolute' />`, () => {
   const view = render(<View position='absolute'></View>)
 
-  expect(view.container).toMatchInlineSnapshot(`
-    .emotion-0 {
-      position: absolute;
-    }
-
-    <div>
-      <div
-        class="emotion-0"
-      />
-    </div>
-  `)
+  expect(view.container).toMatchSnapshot()
 })
