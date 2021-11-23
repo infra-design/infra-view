6ece103ab20eb6118232e917f98fdef540b7514b
limichange <limichange@hotmail.com>
2021-11-23 10:11:13 +0800

    feat: test base props

--- src/components/View/View.ts
+++ src/components/View/View.ts
@@ -2,16 +2,19 @@ import { jsx } from '@emotion/react'
 import { CSSProperties, FC } from 'react'
 
 export interface ViewProps {
+  id?: string
   as?: string
   position?: CSSProperties['position']
 }
 
 export const View: FC<ViewProps> = (props) => {
-  const { as = 'div' } = props
+  const { as = 'div', id, position } = props
 
   return jsx(as, {
-    style: {
-      display: 'flex',
+    id,
+    css: {
+      position,
     },
+    style: {},
   })
 }
--- test/View.test.tsx
+++ test/View.test.tsx
@@ -1,8 +1,43 @@
 import { render } from '@testing-library/react'
+import React from 'react'
 import { View } from '../src'
 
 test('render default View component', async () => {
-  render(<View></View>)
+  const view = render(<View></View>)
 
-  expect(true).toBe(true)
+  expect(view.container).toMatchInlineSnapshot(`
+    <div>
+      <div
+        class="emotion-0"
+      />
+    </div>
+  `)
+})
+
+test('render View as button', () => {
+  const view = render(<View as='button'></View>)
+
+  expect(view.container).toMatchInlineSnapshot(`
+    <div>
+      <button
+        class="emotion-0"
+      />
+    </div>
+  `)
+})
+
+test(`<View position='absolute' />`, () => {
+  const view = render(<View position='absolute'></View>)
+
+  expect(view.container).toMatchInlineSnapshot(`
+    .emotion-0 {
+      position: absolute;
+    }
+
+    <div>
+      <div
+        class="emotion-0"
+      />
+    </div>
+  `)
 })
